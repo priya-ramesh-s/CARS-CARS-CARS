c0ece02e590346d4425dcce1f933c400f083d515
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-08-27 16:39:21 +0100

    Added Setter and Getter methods to Car

--- src/com/SystemManagementRentalCar/Car1.java
+++ src/com/SystemManagementRentalCar/Car1.java
@@ -8,9 +8,28 @@ public class Car1 {
     private BigDecimal rentPrice;
     private String id;
 
+    // Constructor
     public Car1(String name, BigDecimal rentPrice, String id) {
         this.carBrand = name;
+        this.available = true;
         this.rentPrice = rentPrice;
         this.id = id;
     }
+
+    // Setter Method
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // Getter Methods
+    public String getCarBrand() { return this.carBrand; }
+    public String getId() { return this.id; }
+    public boolean getAvailable() { return this.available; }
+    public BigDecimal getRentPrice() { return this.rentPrice; }
+
+    
+
+
+
+
 }
--- src/com/SystemManagementRentalCar/CarManagement.java
+++ src/com/SystemManagementRentalCar/CarManagement.java
@@ -1,7 +1,5 @@
 package com.SystemManagementRentalCar;
 
 public class CarManagement {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
+
 }
